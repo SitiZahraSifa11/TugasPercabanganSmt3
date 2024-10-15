93e4f22ab0fb16372ea6ffe79ae8f27013a2bce7
SitiZahraSifa11 <160223744+SitiZahraSifa11@users.noreply.github.com>
2024-10-15 19:45:20 +0700

    Update CalomProgremer.js

--- CalomProgremer.js
+++ CalomProgremer.js
@@ -1,33 +1,33 @@
-let nilaiCoding = 20 
-let nilaiInterview = "B"
-let hasilCoding = ""
-let hasilInterview = ""
+let nilaiCoding = 20;
+let nilaiInterview = "B";
+let hasilCoding = "";
+let hasilInterview = "";
 
 
 if (nilaiCoding > 80 && nilaiCoding <= 100) {
-    hasilCoding = "LOLOS"
-    console.log("Hasil Tes Coding : "+hasilCoding)
+    hasilCoding = "LOLOS";
+    console.log("Hasil Tes Coding : "+hasilCoding);
 }
 else if (nilaiCoding >= 60 && nilaiCoding<=80) {
-    hasilCoding="DI PERTIMBANGKAN"
-    console.log("Hasil Tes Coding : " + hasilCoding)
+    hasilCoding="DI PERTIMBANGKAN";
+    console.log("Hasil Tes Coding : " + hasilCoding);
 } 
 else {
-    hasilCoding="GAGAL"
-    console.log("Hasil Tes Coding : "+hasilCoding)
+    hasilCoding="GAGAL";
+    console.log("Hasil Tes Coding : "+hasilCoding);
 }
 
- //kondisi yang akan di tatapkan untuk hasil interview
+ 
 if (nilaiInterview =="A" || nilaiInterview =="B") {
-    hasilInterview = "LOLOS"
-    console.log ("Hasil Interview : " + hasilInterview)
+    hasilInterview = "LOLOS";
+    console.log ("Hasil Interview : " + hasilInterview);
 } else {
-    hasilInterview = "GAGAL"
-    console.log ("Hasil Interview : "+hasilInterview)
+    hasilInterview = "GAGAL";
+    console.log ("Hasil Interview : "+hasilInterview);
 }
-// pernyataan perulangan kembali dan output yang di keluar kan
+
 if ((hasilCoding == "LOLOS" || hasilCoding=="DI PERTIMBANGKAN") && hasilInterview =="LOLOS") {
-    console.log ("Selamat Kamu Berhasil Menjadi Calon Programmer")
+    console.log ("Selamat Kamu Berhasil Menjadi Calon Programmer");
 } else {
     console.log("Maaf Kamu Belum Berhasil Menjadi Calon Programmer" );
 }
