d324700f3a67ff246e09af57dec533e5b486ae5f
SitiZahraSifa11 <160223744+SitiZahraSifa11@users.noreply.github.com>
2024-10-15 19:45:43 +0700

    Update DeteksiMafia.js

--- DeteksiMafia.js
+++ DeteksiMafia.js
@@ -1,15 +1,15 @@
-nama = "Zahra"
-umur = 19
-tempatTinggal = "Boston"
-tabungan = 500000
+nama = "Zahra";
+umur = 19;
+tempatTinggal = "Boston";
+tabungan = 500000;
 
 
 if (umur > 40 && (tempatTinggal== "Nevada" || tempatTinggal== "New York" || tempatTinggal== "Havana") && tabungan > 10000000) {
-    console.log(nama + ' kemungkinan adalah seorang anggota mafia dengan pangkat Don' )
+    console.log(nama + ' kemungkinan adalah seorang anggota mafia dengan pangkat Don' );
 }else if (umur <= 40 && umur >= 25 && (tempatTinggal == "New Jersey" || tempatTinggal == "Manhattan" || tempatTinggal == "Nevada")&& tabungan >= 1000000 && tabungan <= 2000000 ){
-    console.log(nama + ' kemungkinan adalah seorang anggota mafia dengan pangkat Underboss')
+    console.log(nama + ' kemungkinan adalah seorang anggota mafia dengan pangkat Underboss');
 }else if (umur <= 24 && umur >= 18 && (tempatTinggal == "California" || tempatTinggal=="Detroit" || tempatTinggal == "Boston")&& tabungan < 1000000 ){
-    console.log(nama + ' kemungkinan adalah seorang anggota mafia dengan pangkat Capo')
+    console.log(nama + ' kemungkinan adalah seorang anggota mafia dengan pangkat Capo');
 }else{
-    console.log(nama + ' Tidak mencurigakan')
+    console.log(nama + ' Tidak mencurigakan');
 }
