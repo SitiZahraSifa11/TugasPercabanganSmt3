9c59a8eb032ddba11aabb5255a392708676920e0
SitiZahraSifa11 <160223744+SitiZahraSifa11@users.noreply.github.com>
2024-10-15 19:46:28 +0700

    Update PersegiFC.js

--- PersegiFC.js
+++ PersegiFC.js
@@ -1,30 +1,30 @@
-let noPung = 60
-let posisi1 = ""
-let posisi2 = ""
-let posisi3 = ""
+let noPung = 60;
+let posisi1 = "";
+let posisi2 = "";
+let posisi3 = "";
 
 if (noPung %2 == 0 && noPung >= 50 && noPung <= 100){
-    posisi1 = "Targer Attacker"
-    posisi2 = "Berhak dipilih menjadi Captain Team"
+    posisi1 = "Targer Attacker";
+    posisi2 = "Berhak dipilih menjadi Captain Team";
 }
 else if(noPung % 2 === 0){
-    posisi1 = "Target Attacker"
+    posisi1 = "Target Attacker";
 }
 else if(noPung % 2 === 1 && noPung%3 ===0 && noPung % 5 === 0 && noPung > 90){
-    posisi1 = "Defender"
-    posisi2 = "Keeper"
-    posisi3 = "Playmaker"
+    posisi1 = "Defender";
+    posisi2 = "Keeper";
+    posisi3 = "Playmaker";
 }
 else if (noPung %2===1 && noPung %3 === 0 && noPung%5 === 0){
-    posisi1 = "Defender"
-    posisi2 = "Keeper"
+    posisi1 = "Defender";
+    posisi2 = "Keeper";
 }
 else if(noPung % 2===1 && noPung > 90){
-    posisi1 = "Defender"
-    posisi2 = "Playmaker"
+    posisi1 = "Defender";
+    posisi2 = "Playmaker";
 }
 else if(noPung % 2 ===1){
-    posisi1 = "Defender"
+    posisi1 = "Defender";
 }
 console.log(
     "Nomor punggung "+noPung+" digunakan oleh posisi:\n"+
